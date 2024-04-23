d9560bfc06b1bb2a5672e1d35aaf1d79f694f2e2
raulgarcia22grados <raul@22grados.com>
2024-04-23 22:58:59 +0100

    My first commit

---
+++ src/components/react/Cursor.jsx
@@ -0,0 +1,8 @@
+"use client";
+import App from '@/components/react/ui/Cursor'; // Ajusta la ruta de importación según tu estructura de directorios
+
+export function AppDemo() {
+  return (
+    <App />
+  );
+}
