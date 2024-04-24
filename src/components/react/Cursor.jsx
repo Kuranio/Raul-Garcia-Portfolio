7257b9514800a935e0196a685e97f2d165890f8b
raulgarcia22grados <raul@22grados.com>
2024-04-24 20:25:29 +0100

    Added about design and functionality

--- src/components/react/Cursor.jsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import App from '@/components/react/ui/Cursor'; // Ajusta la ruta de importación según tu estructura de directorios
-
-export function AppDemo() {
-  return (
-    <App />
-  );
-}
