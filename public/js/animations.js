86dc5187878ae6a78ab9cb620d7903c84d325c0f
raulgarcia22grados <raul@22grados.com>
2024-09-10 13:12:55 +0100

    fixed latest changes

---
+++ public/js/animations.js
@@ -0,0 +1,140 @@
+function initializeAnimations() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  function createScrollAnimation(
+    element,
+    triggerSelector,
+    getScrollAmount,
+    startPosition = "top 0%"
+  ) {
+    const target = document.querySelector(element);
+    if (!target) return;
+
+    const tween = gsap.to(target, {
+      x: getScrollAmount,
+      duration: 3,
+      ease: "none",
+    });
+
+    ScrollTrigger.create({
+      trigger: triggerSelector,
+      start: startPosition,
+      end: () => `+=${getScrollAmount() * -1}`,
+      pin: true,
+      animation: tween,
+      scrub: 1,
+      invalidateOnRefresh: true,
+    });
+  }
+
+  createScrollAnimation(
+    ".races",
+    ".racesWrapper",
+    () => {
+      let racesWidth = document.querySelector(".races").scrollWidth;
+      let multiplier = window.innerWidth >= 768 ? 1 : 1.1;
+      return -(racesWidth - window.innerWidth * multiplier);
+    },
+    "top 0%" // Cambia aquí el valor de inicio según tu preferencia
+  );
+
+  createScrollAnimation(
+    ".imagesWrapper",
+    ".singleProjectWrapper",
+    () => {
+      let imageWidth = document.querySelector(".imagesWrapper").scrollWidth;
+      let multiplier = window.innerWidth >= 768 ? 0.5 : 0.3;
+      return -(imageWidth - window.innerWidth * multiplier);
+    },
+    "top 0%" // Cambia aquí el valor de inicio según tu preferencia
+  );
+
+  // Animación para el título h1 overlayed
+  animateText(".name-title-overlayed");
+
+  // Animación para el título h2 nonoverlayed
+  animateText(".name-title-nonoverlayed");
+
+  // Animación para el header si existe
+  if (document.querySelector(".xlHeader--left")) {
+    gsap.to(".xlHeader--left .xlHeader__header", {
+      xPercent: -25,
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".xlHeader--left .xlHeader__header",
+        start: "top-=500 center",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
+  }
+
+  // Configuración para el overlay
+  setupOverlay();
+}
+
+$(document).on("astro:page-load", initializeAnimations);
+
+function animateText(selector) {
+  var hElement = $(selector);
+  if (!hElement.length) return;
+
+  var text = hElement.text().trim();
+  var letters = text.split("");
+  var delayMultiplier = 150;
+
+  hElement.empty();
+
+  $.each(letters, function (index, letter) {
+    var span = $("<span>").text(letter);
+    span.attr("data-aos", "fade-up");
+    span.attr("data-aos-duration", "500");
+    span.attr("data-aos-delay", (index * delayMultiplier).toString());
+    hElement.append(span);
+  });
+}
+
+function setupOverlay() {
+  const overlay = document.querySelector(".overlay");
+  if (!overlay) return;
+
+  const customOverlay = document.querySelector("#custom-overlay");
+  const circlePath = document.querySelector("#circle-path");
+
+  let mouseX = window.innerWidth / 2;
+  let mouseY = window.innerHeight / 2;
+
+  function updateOverlayPosition() {
+    const rect = overlay.getBoundingClientRect();
+    const x = ((mouseX - rect.left) / rect.width) * 100;
+    const y = ((mouseY - rect.top) / rect.height) * 100;
+
+    gsap.to(overlay, {
+      "--x": `${x}%`,
+      "--y": `${y}%`,
+    });
+
+    requestAnimationFrame(updateOverlayPosition);
+  }
+
+  overlay.style.opacity = "0";
+
+  document.addEventListener("mousemove", (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+
+    overlay.style.opacity = "1";
+  });
+
+  customOverlay.addEventListener("mouseleave", () => {
+    circlePath.style.clipPath = "circle(0px at var(--x, 50%) var(--y, 50%))";
+  });
+
+  customOverlay.addEventListener("mouseenter", () => {
+    circlePath.style.clipPath = "circle(100px at var(--x, 50%) var(--y, 50%))";
+  });
+
+  updateOverlayPosition();
+}
+
+AOS.init();
